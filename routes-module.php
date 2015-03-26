93f516448ecd80f594dc4252036ab73ac507c95a
xLink <xLink@cybershade.org>
2015-03-26 20:26:04 +0000

    updated routebinds

--- routes-module.php
+++ routes-module.php
@@ -6,7 +6,7 @@ $namespace .= '\Module';
 Route::group(['prefix' => 'qdb'], function () use ($namespace) {
 
     Route::group(['prefix' => '{channel}'], function () use ($namespace) {
-        Route::get('{quoteid}', ['as' => 'pxcms.qdb.view', 'uses' => $namespace.'\ViewQuoteController@getQuoteById']);
+        Route::get('{qdb_quote_id}', ['as' => 'pxcms.qdb.view', 'uses' => $namespace.'\ViewQuoteController@getQuoteById']);
         Route::get('/', ['as' => 'pxcms.qdb.channel', 'uses' => $namespace.'\ViewQuoteController@getByChannel']);
     });
 
